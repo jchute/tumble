bbab8bab027b9feb63b7c0365a30cabce963ca91
jchute <jchute@resulta.com>
2021-04-01 12:50:41 -0300

    Tumble Build method added

--- tumble.js
+++ tumble.js
@@ -13,7 +13,17 @@ class Tumble {
     // Initialize Variables and Settings
     this.element = element;
     this.props = {
-      // TODO: Add Options
+      class: 'tumble',
+      classControls: 'tumble__controls',
+      classPagination: 'tumble__pagination',
+      classPaginationActive: 'tumble__pagination--active',
+      classSlide: 'tumble__slide',
+      classSlideActive: 'tumble__slide--active',
+      controlNext: '<button class="tumble__controls--next" aria-label="Next Slide"></button>',
+      controlPrev: '<button class="tumble__controls--prev" aria-label="Previous Slide"></button>',
+      controlPaginationLabel: 'Show Slide',
+      showControls: true,
+      showPagination: true,
       ...props
     }
 
@@ -23,8 +33,61 @@ class Tumble {
       return;
     }
 
-    // TODO: Complete Initialization
+    this.build();
 
+  }
+
+  build() {
+    this.addClasses( this.element, this.props );
+    this.addControls( this.element, this.props );
+  }
+
+  addClasses( element, props ) {
+    // Add Classes
+    element.classList.add( props.class );
+    element.firstElementChild.classList.add( props.classSlideActive );
+
+    Array.from( element.children ).forEach( ( child, i ) => {
+      child.classList.add( props.classSlide );
+      child.dataset.slide = i;
+    } );
+  }
+
+  addControls( element, props ) {
+    const controls = document.createElement( 'nav' );
+    controls.classList.add( props.classControls );
+
+    // Add Next/Prev Controls
+    if ( props.showControls ) {
+      controls.innerHTML += props.controlPrev;
+      controls.innerHTML += props.controlNext;
+    }
+
+    // Add Pagination Controls 
+    if ( props.showPagination ) {
+      const ul = document.createElement( 'ul' );
+      ul.classList.add( props.classPagination );
+
+      for ( let i = 0; i < element.children.length; i++ ) {
+        const li = document.createElement( 'li' );
+        const button = document.createElement( 'button' );
+        button.setAttribute( 'aria-label', `${props.controlPaginationLabel} ${i}` );
+        button.dataset.slide = i;
+
+        if ( i == 0 ) {
+          li.classList.add( props.classPaginationActive );
+        }
+
+        li.appendChild( button );
+        ul.appendChild( li );
+      }
+
+      controls.appendChild( ul );
+    }
+
+    if ( controls.children.length ) {
+      element.appendChild( controls );
+    }
   }
 
 }
